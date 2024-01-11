e0fa9bf14d6238278d2c033c2ee4de6242219bcf
panaud.bastien <bast.panaud@gmail.com>
2024-01-11 18:01:38 +0100

    Ajout pipeline et requirements

--- monitor/dashboard/apps.py
+++ monitor/dashboard/apps.py
@@ -1,6 +1,9 @@
+"""
+Configuration de l'application tableau de bord.
+"""
 from django.apps import AppConfig
-
 
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
+    
