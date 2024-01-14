191a34d4f942699734064b3470b4f13ec8d6781d
CHEKIR <cherifa.chekir@esprit.tn>
2024-01-14 23:24:28 +0100

    add disk usage info to page dynamic metrics

--- monitor/dashboard/urls.py
+++ monitor/dashboard/urls.py
@@ -1,7 +1,7 @@
 # dashboard/urls.py
 
 from django.urls import path
-from .views import pie_chart,cpu_data,  home, dashboard,Dym_metrics,ram_data, staticinfo
+from .views import pie_chart, cpu_data, home, dashboard, Dym_metrics, ram_data, staticinfo, disk_data
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('Dym_metrics/', Dym_metrics, name='Dym_metrics'),
     path('ram_data/', ram_data, name='ram_data'),
     path('cpu_data/', cpu_data, name='cpu_data'),
+    path('disk_data/', disk_data, name='disk_data'),
 
     path('staticinfo/', staticinfo, name='staticinfo'),
     # autres patterns d'URL ici
