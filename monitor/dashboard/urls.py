5d37fd23ed9624ae1009b2cfdd9dec3527aaf29f
Corentin <corentin.laval@etu.univ-st-etienne.fr>
2024-01-14 17:41:32 +0100

    fin avant travail

--- monitor/dashboard/urls.py
+++ monitor/dashboard/urls.py
@@ -1,14 +1,15 @@
 # dashboard/urls.py
 
 from django.urls import path
-from .views import pie_chart,  home, dashboard,ram_chart,ram_data
+from .views import pie_chart,  home, dashboard,ram_chart,ram_data, staticinfo
 
 
 urlpatterns = [
     path('pie_chart/', pie_chart, name='pie_chart'),
-    path('home/', home, name='home' ),
+    path('home/', home, name='home'),
     path('', dashboard, name='dashboard'),
     path('ram_chart/', ram_chart, name='ram_chart'),
-    path('ram_data/', ram_data, name='ram_data'), 
+    path('ram_data/', ram_data, name='ram_data'),
+    path('staticinfo/', staticinfo, name='staticinfo'),
     # autres patterns d'URL ici
 ]
